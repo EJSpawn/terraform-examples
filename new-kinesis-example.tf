9cbd9c025f4308d351b219c9f82eae8e7f66f549
Elvis Justino da SIlva <elvis.justino@hotmail.com>
2023-05-03 21:20:02 -0300

    fix

--- new-kinesis-example.tf
+++ new-kinesis-example.tf
@@ -1,31 +1,32 @@
 resource "aws_kinesis_firehose_delivery_stream" "json_delivery_stream" {
   name        = "my-json-delivery-stream"
-  destination = "glue"
-
+  destination = "s3"
   s3_configuration {
     role_arn            = "arn:aws:iam::123456789012:role/s3-role"
     bucket_arn          = "arn:aws:s3:::my-bucket"
+    prefix              = "sns-events/"
     buffer_size         = 128
     buffer_interval     = 300
     compression_format  = "GZIP"
     error_output_prefix = "error/"
-  }
-
-  extended_s3_configuration {
-    role_arn         = "arn:aws:iam::123456789012:role/s3-role"
-    bucket_arn       = "arn:aws:s3:::my-bucket"
-    prefix           = "sns-events/"
-    error_output_prefix = "error/"
     data_format_conversion_configuration {
-      schema_configuration {
-        database_name = "my_database"
-        table_name    = "my_table"
-        role_arn      = aws_iam_role.example.arn
-      }
       input_format_configuration {
         deserializer {
           open_x_json_ser_de {
             convert_dots_in_json_keys_to_underscores = true
+          }
+        }
+        schema_configuration {
+          database_name = "my_database"
+          table_name    = "my_table"
+          region        = "us-east-1"
+          version_id    = "LATEST"
+          role_arn      = "arn:aws:iam::123456789012:role/glue-role"
+        }
+        input_column_configuration {
+          column_name = "message"
+          mapping_parameters {
+            json_path = "$.Message"
           }
         }
       }
@@ -37,17 +38,5 @@ resource "aws_kinesis_firehose_delivery_stream" "json_delivery_stream" {
         }
       }
     }
-  }  
-}
-
-
-data_format_conversion_configuration {
-      input_format_configuration {
-        deserializer {
-          open_x_json_ser_de {
-            convert_dots_in_json_keys_to_underscores = true
-            field_paths                               = ["$.Message"]
-          }
-        }
-      }
+  }
 }
