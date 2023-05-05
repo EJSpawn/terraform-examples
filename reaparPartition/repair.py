0ca8f008b5cab05dad3d0bd45ca75da9c6300756
Elvis Justino da SIlva <elvis.justino@hotmail.com>
2023-05-04 21:08:26 -0300

    fix

--- reaparPartition/repair.py
+++ reaparPartition/repair.py
@@ -19,49 +19,6 @@ def reparar_particoes(event, context):
             Bucket=bucket_name,
             Prefix=partitions_directory
         )
-        partition_directories = [obj['Key'] for obj in response['Contents']]
-
-        # Extrair as datas (ano_mes_dia) das partições
-        partitions = [os.path.basename(directory) for directory in partition_directories]
-
-        # Adicionar partições no Glue
-        for ano_mes_dia in partitions:
-            partition_location = f"s3://{bucket_name}/{partitions_directory}/{ano_mes_dia}"
-            glue.create_partition(
-                DatabaseName=database_name,
-                TableName=table_name,
-                PartitionInput={
-                    'Values': [ano_mes_dia],
-                    'StorageDescriptor': {
-                        'Location': partition_location,
-                    }
-                }
-            )
-        print(f"Partições adicionadas: {partitions}")
-    except ClientError as e:
-        print(f"Erro ao adicionar partições: {e}")
-
-import os
-import boto3
-from botocore.exceptions import ClientError
-
-def reparar_particoes(event, context):
-    # Configurar o cliente do Glue
-    glue = boto3.client('glue')
-    
-    # Nome da tabela e caminho das partições
-    table_name = event['table_name']
-    partitions_directory = event['partitions_directory']
-    database_name = event['database_name']
-    s3 = boto3.client('s3')
-    bucket_name = event['bucket_name']
-    
-    try:
-        # Listar objetos no caminho das partições
-        response = s3.list_objects_v2(
-            Bucket=bucket_name,
-            Prefix=partitions_directory
-        )
         partition_directories = [obj['Key'] for obj in response['Contents'] if obj['Key'].endswith('/')]
 
         # Extrair as datas (ano_mes_dia) das partições
@@ -77,6 +34,18 @@ def reparar_particoes(event, context):
                     'Values': [ano_mes_dia],
                     'StorageDescriptor': {
                         'Location': partition_location,
+                        'InputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat',
+                        'OutputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat',
+                        'SerdeInfo': {
+                            'SerializationLibrary': 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe',
+                            'Parameters': {
+                                'serialization.format': '1'
+                            }
+                        },
+                        'Columns': [
+                            # Defina as colunas do seu esquema aqui
+                            {'Name': 'example_column', 'Type': 'string'}
+                        ]
                     }
                 }
             )
