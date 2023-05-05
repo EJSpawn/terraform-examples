d54bffb0c256e4d2c29f79e6c9bc772c6b927230
Elvis Justino da SIlva <elvis.justino@hotmail.com>
2023-05-04 21:32:50 -0300

    fix

--- reaparPartition/repair.py
+++ reaparPartition/repair.py
@@ -24,31 +24,40 @@ def reparar_particoes(event, context):
         # Extrair as datas (ano_mes_dia) das partições
         partitions = [os.path.dirname(directory).split('=')[1] for directory in partition_directories]
 
-        # Adicionar partições no Glue
+        # Obter partições existentes
+        existing_partitions = glue.get_partitions(DatabaseName=database_name, TableName=table_name)
+        existing_partition_values = [part['Values'][0] for part in existing_partitions['Partitions']]
+
+        # Adicionar ou atualizar partições no Glue
         for ano_mes_dia in partitions:
             partition_location = f"s3://{bucket_name}/{partitions_directory}/ano_mes_dia={ano_mes_dia}"
-            glue.create_partition(
-                DatabaseName=database_name,
-                TableName=table_name,
-                PartitionInput={
-                    'Values': [ano_mes_dia],
-                    'StorageDescriptor': {
-                        'Location': partition_location,
-                        'InputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat',
-                        'OutputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat',
-                        'SerdeInfo': {
-                            'SerializationLibrary': 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe',
-                            'Parameters': {
-                                'serialization.format': '1'
-                            }
-                        },
-                        'Columns': [
-                            # Defina as colunas do seu esquema aqui
-                            {'Name': 'example_column', 'Type': 'string'}
-                        ]
-                    }
+            partition_input = {
+                'Values': [ano_mes_dia],
+                'StorageDescriptor': {
+                    'Location': partition_location,
+                    'InputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat',
+                    'OutputFormat': 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat',
+                    'SerdeInfo': {
+                        'SerializationLibrary': 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe',
+                        'Parameters': {
+                            'serialization.format': '1'
+                        }
+                    },
+                    'Columns': [
+                        # Defina as colunas do seu esquema aqui
+                        {'Name': 'example_column', 'Type': 'string'}
+                    ]
                 }
-            )
-        print(f"Partições adicionadas: {partitions}")
+            }
+
+            # Verifica se a partição já existe
+            if ano_mes_dia in existing_partition_values:
+                # Se a partição existir, atualiza a partição
+                glue.update_partition(DatabaseName=database_name, TableName=table_name, PartitionValueList=[ano_mes_dia], PartitionInput=partition_input)
+            else:
+                # Se a partição não existir, cria a partição
+                glue.create_partition(DatabaseName=database_name, TableName=table_name, PartitionInput=partition_input)
+
+        print(f"Partições adicionadas ou atualizadas: {partitions}")
     except ClientError as e:
-        print(f"Erro ao adicionar partições: {e}")
+        print(f"Erro ao adicionar ou atualizar partições: {e}")
