43154e8cfaae41cb58addf8928fee6486ddf39af
Elvis Justino da SIlva <elvis.justino@hotmail.com>
2023-05-05 18:08:16 -0300

    fix

--- reaparPartition/testRepair2.py
+++ reaparPartition/testRepair2.py
@@ -23,12 +23,11 @@ def setup_glue_and_s3():
             s3.create_bucket(Bucket="test-bucket")
             yield glue, s3
 
-def test_reparar_particoes(setup_glue_and_s3):
+def test_create_partition(setup_glue_and_s3):
     glue, s3 = setup_glue_and_s3
 
-    # Criar partições S3
+    # Criar partição S3
     s3.put_object(Bucket="test-bucket", Key="test_part/ano_mes_dia=20220101/")
-    s3.put_object(Bucket="test-bucket", Key="test_part/ano_mes_dia=20220102/")
 
     event = {
         "table_name": "test_table",
@@ -38,21 +37,15 @@ def test_reparar_particoes(setup_glue_and_s3):
     }
     reparar_particoes(event, None)
 
-    # Verificar se as partições foram adicionadas
+    # Verificar se a partição foi adicionada
     partitions = glue.get_partitions(DatabaseName="test_db", TableName="test_table")["Partitions"]
-    assert len(partitions) == 2
-    assert {"Values": ["20220101"]} in partitions
-    assert {"Values": ["20220102"]} in partitions
+    assert len(partitions) == 1
+    partition = partitions[0]
 
-    # Atualizar partições S3
-    s3.put_object(Bucket="test-bucket", Key="test_part/ano_mes_dia=20220102/")
-    s3.put_object(Bucket="test-bucket", Key="test_part/ano_mes_dia=20220103/")
-
-    reparar_particoes(event, None)
-
-    # Verificar se as partições foram atualizadas e a nova partição foi adicionada
-    partitions = glue.get_partitions(DatabaseName="test_db", TableName="test_table")["Partitions"]
-    assert len(partitions) == 3
-    assert {"Values": ["20220101"]} in partitions
-    assert {"Values": ["20220102"]} in partitions
-    assert {"Values": ["20220103"]} in partitions
+    assert partition["Values"] == ["20220101"]
+    assert partition["StorageDescriptor"]["Location"] == "s3://test-bucket/test_part/ano_mes_dia=20220101"
+    assert partition["StorageDescriptor"]["InputFormat"] == "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    assert partition["StorageDescriptor"]["OutputFormat"] == "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+    assert partition["StorageDescriptor"]["SerdeInfo"]["SerializationLibrary"] == "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+    assert partition["StorageDescriptor"]["SerdeInfo"]["Parameters"]["serialization.format"] == "1"
+    assert partition["StorageDescriptor"]["Columns"] == [{"Name": "example_column", "Type": "string"}]
